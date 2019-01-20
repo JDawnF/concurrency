964cfca55d20e9905f952798e07673faddf9277e
JDawnF <335825732@qq.com>
2019-01-20 21:16:14 +0800

    注解

---
+++ src/main/java/com/baichen/concurrency/annotations/ThreadSafe.java
@@ -0,0 +1,18 @@
+package com.baichen.concurrency.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @program: concurrency
+ * @description: 线程安全类标识注解
+ * @author: baichen
+ * @create: 2019-01-20 21:08
+ **/
+@Target(ElementType.TYPE)       //注解作用的目标，这里是对一个类作为注解
+@Retention(RetentionPolicy.SOURCE)        //注解存在的范围,编译的时候会被忽略
+public @interface ThreadSafe {
+    String value() default "";
+}
