efc7462c318834383aa89df2ada95b06cc634a21
JDawnF <335825732@qq.com>
2019-01-27 22:44:15 +0800

    过滤器

---
+++ src/main/java/com/baichen/concurrency/HttpFilter.java
@@ -0,0 +1,36 @@
+package com.baichen.concurrency;
+
+import com.baichen.concurrency.example.threadLocal.RequestHolder;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+/**
+ * @program: concurrency
+ * @description:
+ * @author: baichen
+ **/
+@Slf4j
+public class HttpFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        //打印当前线程的ID与请求路径
+        log.info("do filter, {}, {}", Thread.currentThread().getId(), request.getServletPath());
+        //将当前线程ID添加到ThreadLocal中
+        RequestHolder.add(Thread.currentThread().getId());
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
