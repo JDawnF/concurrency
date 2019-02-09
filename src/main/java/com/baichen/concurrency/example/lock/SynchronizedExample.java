c9a597f7a667867c436103c49b2f73dc71e0b952
JDawnF <335825732@qq.com>
2019-02-09 12:23:08 +0800

    Synchronized例子

---
+++ src/main/java/com/baichen/concurrency/example/lock/SynchronizedExample.java
@@ -0,0 +1,52 @@
+package com.baichen.concurrency.example.lock;
+
+import com.baichen.concurrency.annotations.ThreadSafe;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @program: concurrency
+ * @description: 并发测试
+ * @author: baichen
+ * @create: 2019-01-20 22:03
+ **/
+
+@Slf4j
+@ThreadSafe
+public class SynchronizedExample {
+    //请求总数
+    public static int clientTotal = 5000;
+    //同时并发执行的线程数
+    public static int threadTotal = 200;
+    public static AtomicInteger count = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();    //判断当前线程是否允许被执行
+                    add();
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("exception", e);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        log.info("count:{}", count.get());
+    }
+
+    private synchronized static void add() {
+        count.incrementAndGet();
+    }
+}
