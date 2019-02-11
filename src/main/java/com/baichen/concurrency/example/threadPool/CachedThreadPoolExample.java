59e3318623a93e38f63c961f544efde3bf9875ec
JDawnF <335825732@qq.com>
2019-02-11 23:56:47 +0800

    no message

---
+++ src/main/java/com/baichen/concurrency/example/threadPool/CachedThreadPoolExample.java
@@ -0,0 +1,27 @@
+package com.baichen.concurrency.example.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @program: concurrency
+ * @author: baichen
+ **/
+@Slf4j
+public class CachedThreadPoolExample {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("task:{}", index);
+                }
+            });
+        }
+        executorService.shutdown();
+    }
+}
